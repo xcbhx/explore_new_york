79f8e69e0284efacf7ae00a900d25c16d74df829
Ceina Ellison <ceina.ellison@gmail.com>
2025-03-06 15:53:26 -0500

    edited pages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,18 @@
-
+import './About.css'
 
 function About() {
   return (
-    <div>
-
+    <div className='About'>
+      <h1>About Us</h1>
+      <img className='About-image' src='/images/about.jpeg' alt='About Page' />
+      <p>Welcome to the Explore New York! 
+        I’ve always loved living in New York and discovering its 
+        hidden gems, from beautiful museums to vibrant street art. 
+        I created this site to share my favorite spots and help others 
+        explore the city’s rich culture, food, and history. Whether 
+        you're a local or a visitor, I hope this guide helps you uncover 
+        new places to love in NYC!
+      </p>
     </div>
   )
 };
--- src/components/Privacy/Privacy.jsx
+++ src/components/Privacy/Privacy.jsx
@@ -1,8 +1,19 @@
-
+import './Privacy.css'
 
 function Privacy() {
   return (
-    <div></div>
+    <div className='Privacy'>
+      <h1>Privacy Policy</h1>
+      <p>
+        Your privacy is important to us. This site may 
+        collect basic browsing data to improve user experience, 
+        but we do not sell or share personal information. Any 
+        location-based features rely on your permission and are used 
+        solely to enhance your exploration of New York. By using this 
+        site, you agree to our limited data collection practices for a 
+        better browsing experience.
+      </p>
+    </div>
   )
 };  
 
